436cff6778db84da70e927054de1d35a94eb72cb
ankomahene <ankomaheneshadrack@gmail.com>
2019-12-15 23:28:07 +0000

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NewStoryComponent } from './components/new-story/new-story.component';
+import { NewChapterComponent } from './components/new-chapter/new-chapter/new-chapter.component';
+import { ChapterComponent } from './components/chapter/chapter/chapter.component';
+import { HomeComponent } from './components/home/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NewStoryComponent,
+    NewChapterComponent,
+    ChapterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/new-chapter/new-chapter/new-chapter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-chapter',
+  templateUrl: './new-chapter.component.html',
+  styleUrls: ['./new-chapter.component.css']
+})
+export class NewChapterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
