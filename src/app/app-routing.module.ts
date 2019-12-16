66ca4925b0a50404422cb6cca309259c857cb705
ankomahene <ankomaheneshadrack@gmail.com>
2019-12-16 07:43:20 +0000

    Added route starter endpoints

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewStoryComponent } from './components/new-story/new-story.component';
+import { NewChapterComponent } from './components/new-chapter/new-chapter.component';
+import { ChapterComponent } from './components/chapter/chapter.component';
+import { HomeComponent } from './components/home/home.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {path: 'index', component: HomeComponent},
+  {path: 'reader', component: ChapterComponent},
+  {path: 'new_story', component: NewStoryComponent},
+  {path: 'new_chapter', component: NewChapterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewStoryComponent } from './components/new-story/new-story.component';
-import { NewChapterComponent } from './components/new-chapter/new-chapter/new-chapter.component';
-import { ChapterComponent } from './components/chapter/chapter/chapter.component';
-import { HomeComponent } from './components/home/home/home.component';
+import { NewChapterComponent } from './components/new-chapter/new-chapter.component';
+import { ChapterComponent } from './components/chapter/chapter.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
